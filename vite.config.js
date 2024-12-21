9befde824c56fdd588142b08970944f4f2c4e972
Emmett Hoolahan <emmetthoolahan@gmail.com>
2024-12-22 00:56:32 +0800

    qwxx
    qwx

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
+// Dynamically import the PWA plugin to handle potential missing dependency
+const plugins = [vue()]
+
+try {
+  const { VitePWA } = await import('vite-plugin-pwa')
+  plugins.push(
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
@@ -22,12 +23,19 @@ export default defineConfig({
             src: 'icons/icon-72x72.png',
             sizes: '72x72',
             type: 'image/png'
-          },
-          // ... other icons from manifest.json
+          }
+          // Other icons will be loaded from manifest.json
         ]
       }
     })
-  ],
+  )
+} catch (e) {
+  console.warn('vite-plugin-pwa not found, PWA features will be disabled')
+}
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
